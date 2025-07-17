903ab6c619fb948811fd8dd713ceb59ec0d5eafd
Elizaveta Logvinenko <elizaveta20logvinenko@gmail.com>
2025-07-17 22:55:15 +0400

    added classes Writer and AppData

---
+++ src/main/java/io/github/lizalogvinenko/lesson_12/AppData.java
@@ -0,0 +1,21 @@
+package io.github.lizalogvinenko.lesson_12;
+
+public class AppData {
+    private String[] header;
+    private int[][] data;
+
+    public AppData(String[] header, int [][] data) {
+        this.header = header;
+        this.data = data;
+    }
+
+    public int[][] getData() {
+        return data;
+    }
+
+    public String[] getHeader() {
+        return header;
+    }
+
+
+}
--- src/main/java/io/github/lizalogvinenko/lesson_12/Main.java
+++ src/main/java/io/github/lizalogvinenko/lesson_12/Main.java
@@ -1,4 +1,20 @@
 package io.github.lizalogvinenko.lesson_12;
 
 public class Main {
+
+    public static void main(String[] arg){
+        String fileName = "my_file_with_information.csv";
+
+        String[] header = {"Num", "Kilo", "Age"};
+
+        int[][] data = {
+                {3, 2, 24},
+                {9, 4, 30}
+        };
+
+        AppData appData = new AppData(header, data);
+        Writer.writeToCSVFile(fileName, appData);
+    }
+
+
 }
